44b068c405274f3d90d5227f007f9211489874f0
Noor Verstraete <noor.verstraete@epfl.ch>
2025-05-11 15:15:40 +0200

    added function to noor2

--- src/molstorage/noor2.py
+++ src/molstorage/noor2.py
@@ -1,4 +1,4 @@
-from noor_checked import *
+"""from noor_checked import *
 
 from noor_checked import extract_all_sections
 
@@ -24,3 +24,43 @@ def extract_storage_specifications(text):
 
 extract_storage_specifications(section_7_text)
 
+"""
+
+from Noor import *
+
+titles7:list[str] = ["Handling","Storage"]
+titles10:list[str] = ["Reactive Hazard","Stability","Conditions to Avoid","Incompatible Materials","Hazardous Decomposition Products","Hazardous Polymerization","Hazardous Reactions"]
+
+def separate_section10_per_title(section,titles)->dict:
+    dictio = {}
+    for i,title in enumerate(titles):
+        if title != titles[len(titles)-1]:
+            if title in section:
+                start_of_info_index = section.index(title)+len(title)
+                end_of_info_index = section.index(titles10[i + 1])
+                info = section[start_of_info_index:end_of_info_index]
+                dictio[title] = info.replace("_","").replace("\n","")
+        else:
+            if title in section:
+                start_of_info_index = section.index(title)+len(title)
+                info = section[start_of_info_index:]
+                dictio[title] = info.replace("_","").replace("\n","")
+    return dictio
+
+def main()->None:
+    chemical1 = input("Enter first chemical name: ")
+    chemical2 = input("Enter second chemical name: ")
+    sds_link1= get_fisher_sds_link(chemical1)
+    sds_link2= get_fisher_sds_link(chemical2)
+    section7, section10_1 = extract_sds_sections(sds_link1)    
+    section7, section10_2 = extract_sds_sections(sds_link2)  
+    dictio_1=separate_section_per_title(section10_1,titles10)
+    dictio_2=separate_section_per_title(section10_2,titles10)
+    dictio_12 = {}
+    for item in dictio_1:
+        dictio_12[dictio_1[item]]=dictio_2[item]
+    for key,value in dictio_12.items():
+        print(f"\n{chemical1}: {key} \n{chemical2}: {value}\n")
+
+if __name__=="__main__":
+    main()
