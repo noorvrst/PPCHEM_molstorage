f8fafc32e59e2dd46fbd333471aa28e99dbaef4d
chacha333-create <charlotte.mercier@epfl.ch>
2025-05-18 12:40:02 +0200

    init change

--- src/molstorage/__init__.py
+++ src/molstorage/__init__.py
@@ -1,9 +1,8 @@
 """Storage of hazardous chemicals based on their pictograms, hazards statements, acid/base classification, state"""
 
-from __future__ import annotations
+# src/molstorage/__init__.py
 
-__version__ = "0.0.1"
+from .molstorage import *
+from .app import *
 
-from .molstorage import get_compound_safety_data, get_name_and_smiles, classify_acid_base, get_mp_bp, compound_state, prioritize_pictograms, is_chemically_compatible, default_group, default_group_gas, initialize_storage_groups, chemsort_multiple_order_3 
-
-from .app_run import App
+__version__ = "0.1.0"
