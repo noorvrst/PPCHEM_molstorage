a8aff87339c51accf1d0868379613a2fa42057e8
chacha333-create <charlotte.mercier@epfl.ch>
2025-05-18 12:56:54 +0200

    init

--- src/molstorage/__init__.py
+++ src/molstorage/__init__.py
@@ -2,7 +2,24 @@
 
 # src/molstorage/__init__.py
 
-from .molstorage import *
-from .app import *
+from .molstorage import *  
 
-__version__ = "0.1.0"
+# Add this function
+def launch_app():
+    """
+    Launches the Streamlit user interface for molstorage.
+    This function starts a local server and opens the application in the browser.
+    """
+    import streamlit.web.cli as stcli
+    import sys
+    import os
+    
+    # Find the path to app.py which is in the same folder as __init__.py
+    dirname = os.path.dirname(__file__)
+    app_path = os.path.join(dirname, 'app.py')
+    
+    # Configure the arguments for streamlit
+    sys.argv = ["streamlit", "run", app_path]
+    
+    # Launch the application
+    stcli.main()
